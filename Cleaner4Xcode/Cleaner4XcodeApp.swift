3804b56ce6fddc857c1e31f382f9d9ff170102f0
Roger Lee <rijie.li@outlook.com>
2026-03-04 14:24:38 +0800

    chore: cleanup

---
+++ Cleaner4Xcode/Cleaner4XcodeApp.swift
@@ -0,0 +1,20 @@
+//
+//  Cleaner4XcodeApp.swift
+//  Cleaner for Xcode
+//
+//  Created by Roger on 2026/3/4.
+//  Copyright © 2026 Baye. All rights reserved.
+//
+
+import Cocoa
+import SwiftUI
+
+@main
+struct Cleaner4XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.hiddenTitleBar)
+    }
+}
